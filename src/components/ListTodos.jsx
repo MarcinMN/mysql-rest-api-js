36f03437d1e549998acde26357d7c43c70383cd2
MarcinMN <79340820+MarcinMN@users.noreply.github.com>
2023-05-29 12:23:43 -0500

    Added Delete Todo functionality

--- src/components/ListTodos.jsx
+++ src/components/ListTodos.jsx
@@ -9,6 +9,17 @@ const ListTodos = () => {
     setTodos(todoArray);
   }
 
+  async function deleteTodo(id) {
+    try {
+      const res = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
   useEffect(() => {
     getTodos();
   }, []);
@@ -30,7 +41,14 @@ const ListTodos = () => {
             <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
